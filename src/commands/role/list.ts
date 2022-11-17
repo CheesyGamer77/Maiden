c563898ea28a6b624e01c6388998488b1871b4bf
Casey <43181046+CheesyGamer77@users.noreply.github.com>
2022-11-17 14:54:02 -0500

    Add `role list` command (#2)
    * chore: re-lint
    
    * feat(commands): `/role list`
    
    * fix: weird sorting
    
    Use role position to sort listed roles

---
+++ src/commands/role/list.ts
@@ -0,0 +1,37 @@
+import { Buffer } from 'node:buffer';
+import { Subcommand } from 'cheesyutils.js';
+import { ChatInputCommandInteraction, CacheType, AttachmentBuilder, EmbedBuilder, Colors } from 'discord.js';
+
+export class RoleListCommand extends Subcommand {
+    constructor() {
+        super('list', 'Lists all the roles in the guild');
+    }
+
+    override async invoke(ctx: ChatInputCommandInteraction<CacheType>): Promise<void> {
+        if (!ctx.inCachedGuild()) {
+            return;
+        }
+
+        const guild = ctx.guild;
+
+        // Sort roles by position
+        const roles = guild.roles.cache.sorted((a, b) => b.position - a.position);
+
+        let bufferString = '';
+
+        for (const [id, role] of roles) {
+            bufferString += `${role.name} - ${id}\n`;
+        }
+        bufferString = bufferString.trim();
+
+        await ctx.reply({
+            embeds: [new EmbedBuilder()
+                .setDescription(`:white_check_mark: Showing ${roles.size} total roles`)
+                .setColor(Colors.Green),
+            ],
+            files: [
+                new AttachmentBuilder(Buffer.from(bufferString, 'utf-8'), { name: `roles-${guild.id}.txt` }),
+            ],
+        });
+    }
+}
